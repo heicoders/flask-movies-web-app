1f121e0e28f4e38b63a6f144f6e577269199a3dc
Min Yan <minyan@heicodersacademy.com>
2020-12-30 14:28:19 +0800

    Render HTML template in app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route("/")
-def homepage():
-	return "Hello World"
+def show_landing_page():
+    return render_template("landing-page.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

5f34c7cc3b7a42b4a0ecc1166e3b101258840615
Min Yan <minyan@heicodersacademy.com>
2020-12-30 13:54:25 +0800

    Get first flask server running

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-import flask
-import requests
+from flask import Flask
 
-print("Import success!")
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+	return "Hello World"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
